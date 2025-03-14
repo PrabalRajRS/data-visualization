6dffae118bb1d22541e2f2868be0975cb5569741
PRABAL RAJ <96192793+PrabalRajRS@users.noreply.github.com>
2025-03-14 18:27:17 +0530

    Update JsonViewer.js

--- src/components/JsonViewer.js
+++ src/components/JsonViewer.js
@@ -1,4 +1,4 @@
-import ReactJson from 'react-json-view';
+import ReactJson from 'react18-json-view';
 
 const JsonViewer = ({ data }) => <div className='chart-container'><h4>Json View</h4><ReactJson src={data} collapsed={1} /></div>;
 
