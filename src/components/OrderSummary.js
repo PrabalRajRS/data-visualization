9e73944a07ed50f66e51e47e3df6038a4c6203af
PRABAL RAJ <96192793+PrabalRajRS@users.noreply.github.com>
2025-03-14 20:05:55 +0530

    Update OrderSummary.js

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -1,5 +1,5 @@
 const OrderSummary = ({ order }) => (
-    <div className='chart-container'>
+    <div className='chart-container' style={{padding: 20}}>
       <h3 style={{marginBottom: 10, color: 'black', fontWeight: "900"}}>Order Summary</h3>
       <p><strong>Order ID:</strong> {order.Order_ID}</p>
       <p><strong>Customer:</strong> {order.Customer_Name}</p>
